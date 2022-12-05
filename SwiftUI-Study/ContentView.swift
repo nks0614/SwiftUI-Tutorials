c4b091deddcab423e6afac99ab7e0ca630792662
GyuSeok Nam <namgyeseok0614@naver.com>
2022-12-05 12:34:41 +0900

    Section 1 ~ 3 1. Create a New Project and Explore the Canvas 2. Customize the TextView 3. Combine Views Using Stacks

---
+++ SwiftUI-Study/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  SwiftUI-Study
+//
+//  Created by nkstar on 2022/12/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Josua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Study/SwiftUI_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StudyApp.swift
+//  SwiftUI-Study
+//
+//  Created by nkstar on 2022/12/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
